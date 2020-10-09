cb9c5da11662ea3294eb205de03f7b5a48889c20
trimai_ <69742343+walshiiro@users.noreply.github.com>
2020-10-09 23:17:29 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,42 @@
+
 #include <bits/stdc++.h>
 using namespace std;
+int main()
+{
+    int n=0,maxx=0,vtmax=0;
+    int a[9999],
+            dp[9999],
+            tv[9999];
+    dp[0]=0;
+    
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        cin>>a[i];
+        dp[i]=1;
+        for(int j=0;j<i;j++)
+        {
+            if(dp[j]>=dp[i]&&a[i]>a[j])
+            {
+                dp[i]=dp[j]+1;
+                tv[i]=j;
+            }
+            if(dp[i]>maxx)
+            {
+                maxx=dp[i];
+                vtmax=i;
+            }
+        }
 
-int main() {
- int x;
- cin>>x;
- cin.ignore();
- int a[x];
- for(int i=0;i<x;i++)
-     cin>>a[i];
- int lmax,l[100];
- l[0]=1;
- for(int i=1;i<x;i++) {
-     lmax=0;
-     for (int j = 0; j < i; j++)
-         if (a[i] > a[j])
-             if (lmax <l[j])
-                 lmax=l[j];
-     l[i]=lmax+1;
- }
 
-cout<<l[x-1];
+    }
+    while(vtmax!=0)
+    {
+        cout<<a[vtmax]<<" ";
+        vtmax=tv[vtmax];
+    }
+
+
 
 
 }
